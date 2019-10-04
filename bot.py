ea55d90dcaa1926e9ecc4ad6b606a4ae7c1f7c2a
Ralph-Joseph Tatt <ralph-joseph.tatt@infinityworks.com>
2019-10-04 19:36:51 +0100

    Fixed print statement

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ def answer_tweets():
     tweets = api.mentions_timeline(since_id=last_tweet)
     if (len(tweets)):
         last_tweet = tweets[-1].id
-        print last_tweet
+        print(last_tweet)
     for tweet in tweets:
 
         tweet = api.get_status(tweet.id,tweet_mode="extended")
